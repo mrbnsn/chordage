d7acd9984e2993e329a0e987f27b550503e15bd6
Mike Robinson <mr.mike.robinson@gmail.com>
2023-11-25 10:39:31 -0500

    added key prop

--- src/components/scale-display/scale-display.js
+++ src/components/scale-display/scale-display.js
@@ -4,11 +4,14 @@ import chordData from '../../data/chord-data';
 import { replaceMusicalQualifiers } from '../../lib/utils';
 
 const ScaleDisplay = ({ root, type }) => {
-    console.log(`root:`, root);
-    console.log(`type:`, type);
     const renderChords = () => (
-        chordData[root]?.[type]?.chords?.map((chord) => <Box dangerouslySetInnerHTML={{ __html: replaceMusicalQualifiers(chord).replace(' ', '') }} />)
-    )
+        chordData[root]?.[type]?.chords?.map((chord) => (
+            <Box 
+                key={chord}
+                dangerouslySetInnerHTML={{ __html: replaceMusicalQualifiers(chord).replace(' ', '') }} 
+            />
+        )
+    ));
 
     return (
         <Box>
