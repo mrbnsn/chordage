475ead7d9f823b1069214b3c8bb82354349dcc5a
Mike Robinson <mr.mike.robinson@gmail.com>
2023-11-25 13:49:17 -0500

    changed file case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App';
 import { CssBaseline } from '@mui/material';
 
 import './index.css';
